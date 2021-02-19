2e08bde57ac84d3891c0824454eb68cdd856433f
Ejike Sylva <sylvaejike@gmail.com>
2021-02-19 17:26:38 +0100

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.divideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiplyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.powerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cLRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecondValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.addToolStripMenuItem,
+            this.subtractToolStripMenuItem,
+            this.divideToolStripMenuItem,
+            this.multiplyToolStripMenuItem,
+            this.powerToolStripMenuItem,
+            this.cLRToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(495, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.AutoSize = false;
+            this.exitToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.addToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(47, 23);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.subtractToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(77, 23);
+            this.subtractToolStripMenuItem.Text = "Subtract";
+            this.subtractToolStripMenuItem.Click += new System.EventHandler(this.subtractToolStripMenuItem_Click);
+            // 
+            // divideToolStripMenuItem
+            // 
+            this.divideToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.divideToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divideToolStripMenuItem.Name = "divideToolStripMenuItem";
+            this.divideToolStripMenuItem.Size = new System.Drawing.Size(59, 23);
+            this.divideToolStripMenuItem.Text = "Divide";
+            this.divideToolStripMenuItem.Click += new System.EventHandler(this.divideToolStripMenuItem_Click);
+            // 
+            // multiplyToolStripMenuItem
+            // 
+            this.multiplyToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.multiplyToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplyToolStripMenuItem.Name = "multiplyToolStripMenuItem";
+            this.multiplyToolStripMenuItem.Size = new System.Drawing.Size(71, 23);
+            this.multiplyToolStripMenuItem.Text = "Multiply";
+            this.multiplyToolStripMenuItem.Click += new System.EventHandler(this.multiplyToolStripMenuItem_Click);
+            // 
+            // powerToolStripMenuItem
+            // 
+            this.powerToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.powerToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerToolStripMenuItem.Name = "powerToolStripMenuItem";
+            this.powerToolStripMenuItem.Size = new System.Drawing.Size(57, 23);
+            this.powerToolStripMenuItem.Text = "Power";
+            this.powerToolStripMenuItem.Click += new System.EventHandler(this.powerToolStripMenuItem_Click);
+            // 
+            // cLRToolStripMenuItem
+            // 
+            this.cLRToolStripMenuItem.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cLRToolStripMenuItem.Name = "cLRToolStripMenuItem";
+            this.cLRToolStripMenuItem.Size = new System.Drawing.Size(42, 23);
+            this.cLRToolStripMenuItem.Text = "CLR";
+            this.cLRToolStripMenuItem.Click += new System.EventHandler(this.cLRToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.PanEast;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(53, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First value";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFirstValue
+            // 
+            this.txtFirstValue.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtFirstValue.Location = new System.Drawing.Point(292, 63);
+            this.txtFirstValue.Multiline = true;
+            this.txtFirstValue.Name = "txtFirstValue";
+            this.txtFirstValue.Size = new System.Drawing.Size(132, 30);
+            this.txtFirstValue.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label2.Cursor = System.Windows.Forms.Cursors.PanEast;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(52, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second value";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSecondValue
+            // 
+            this.txtSecondValue.Location = new System.Drawing.Point(292, 156);
+            this.txtSecondValue.Multiline = true;
+            this.txtSecondValue.Name = "txtSecondValue";
+            this.txtSecondValue.Size = new System.Drawing.Size(132, 30);
+            this.txtSecondValue.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label3.Cursor = System.Windows.Forms.Cursors.PanEast;
+            this.label3.Location = new System.Drawing.Point(53, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Controls.Add(this.txtFirstValue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSecondValue);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 376);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arithmetic Operations";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblResult.Location = new System.Drawing.Point(292, 266);
+            this.lblResult.Multiline = true;
+            this.lblResult.Name = "lblResult";
+            this.lblResult.ReadOnly = true;
+            this.lblResult.Size = new System.Drawing.Size(132, 30);
+            this.lblResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(495, 418);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MEG202.1 Assignment";
+            this.TransparencyKey = System.Drawing.Color.DarkMagenta;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem divideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiplyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem powerToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecondValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox lblResult;
+        private System.Windows.Forms.ToolStripMenuItem cLRToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double Number1, Number2, Result; // declaration of variable of importance
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //method to close the program 
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //method to perform addition of two numbers from the textbox value objects
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            UploadData(); // the upload data method is being called here
+            Result = Number1 + Number2; // Evaluation the result i.e the addition of them both
+            lblResult.Text = Result.ToString();
+        }
+
+        //funtion to help erase all text boxes after and arithmetic operation
+        private void cLRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtFirstValue.Text = " ";
+            txtSecondValue.Text = " ";
+            lblResult.Text = " ";
+        }
+
+        //method to easily import data from the textbox objects
+        private void UploadData()
+        {
+            //an exception block to help handle errors 
+            //like a user mistakenly using a text e.g "hello" in place of a number
+            //to enable the code not to crash :)
+            try
+            {
+                Number1 = Double.Parse(txtFirstValue.Text);
+                Number2 = Double.Parse(txtSecondValue.Text);
+            }
+            catch (FormatException e)
+            {
+                txtFirstValue.Text = "";
+                txtSecondValue.Text = "";
+                lblResult.Text = "";
+            }
+        }
+
+        //method to perfom substraction on two numbers from the textbox value objects
+        private void subtractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UploadData();
+            Result = Number1 - Number2;
+            lblResult.Text = Result.ToString();
+        }
+
+        //method to perfom division of two numbers
+        private void divideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UploadData();
+            Result = Number1 / Number2;
+            lblResult.Text = Result.ToString();
+        }
+
+        //method to perfom multiplication of two numbers
+        private void multiplyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UploadData();
+            Result = Number1 * Number2;
+            lblResult.Text = Result.ToString();
+        }
+
+        //method to perform power / exponential arithmetic operation
+        private void powerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UploadData();
+            Result = Math.Pow(Number1, Number2);
+            lblResult.Text = Result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
